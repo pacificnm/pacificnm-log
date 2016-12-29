8181527190a7238daeacaa77497721a18a6356fe
Jaimie Garner <pacificnm@gmail.com>
2016-12-29 10:34:39 -0800

    updates readme and configs

--- config/module.config.php
+++ config/module.config.php
@@ -10,7 +10,7 @@ return array(
     'module' => array(
         'Log' => array(
             'name' => 'Log',
-            'version' => '1.0.3',
+            'version' => '1.0.4',
             'install' => array()
         )
     ),
